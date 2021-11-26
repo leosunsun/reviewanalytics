0fd0e7472458e4583f25ad3778661c725fdd310d
leosunsun <Leo@Leo-Huang.local>
2021-11-26 11:34:34 +0700

    version2_calculate

--- review.py
+++ review.py
@@ -6,7 +6,9 @@ with open ('reviews.txt', 'r') as f:
 		count += 1  #count=count+1 的快写法
 		if count % 1000 == 0: #%为整除符号，这里表示的是：if count除以1000的余数为0
 			print (len(data))
-print(len(data))
-print(data[0])
-print('------------')
-print(data[1])
+print ('档案读取完毕，共有', len(data),'条留言')
+
+sum_len = 0
+for d in data: 
+	sum_len = sum_len + len(d)
+print('留言的平均长度为', sum_len/len(data)) 
